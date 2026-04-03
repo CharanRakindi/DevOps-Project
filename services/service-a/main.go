1a4b2a9a4d66f9dd712e5e5cdf09a108446adca5
CharanRakindi <charanrakindi@gmail.com>
2026-04-03 19:56:52 +0530

    Calls service-b

--- services/service-a/main.go
+++ services/service-a/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	r.GET("/api", func(c *gin.Context) {
 		// 🔥 CALL SERVICE-B
-		resp, err := http.Get("http://service-b")
+		resp, err := http.Get("http://service-b/api")
 
 		var serviceBResponse string
 
@@ -32,9 +32,9 @@ func main() {
 
 		// FINAL RESPONSE
 		c.JSON(200, gin.H{
-			"message": "Hello from Service A",
-			"service": "service-a",
-			"version": version,
+			"message":   "Hello from Service A",
+			"service":   "service-a",
+			"version":   version,
 			"service_b": serviceBResponse,
 		})
 	})
@@ -43,7 +43,7 @@ func main() {
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "healthy", "service": "service-a", "version": version})
 	})
-	
+
 	r.GET("/version", func(c *gin.Context) {
 		c.JSON(200, gin.H{"service": "service-a", "version": version})
 	})
